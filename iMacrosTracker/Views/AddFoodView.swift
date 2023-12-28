94c9de5c10e2606834f149b90234c4fdbbe080da
Jonathan Tong <jontong321@gmail.com>
2023-12-28 11:10:41 -0500

    Completed timeformatting and addfoodview files

--- iMacrosTracker/Views/AddFoodView.swift
+++ iMacrosTracker/Views/AddFoodView.swift
@@ -4,5 +4,44 @@
 //
 //  Created by Jonathan on 2023-12-28.
 //
+import SwiftUI
 
-import Foundation
+struct AddFoodView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var calories: Double = 0
+    
+    var body: some View {
+            Form {
+                Section() {
+                    TextField("Food name", text: $name)
+                    
+                    VStack {
+                        Text("Calories: \(Int(calories))")
+                        Slider(value: $calories, in: 0...1000, step: 10)
+                    }
+                    .padding()
+                    
+                    HStack {
+                        Spacer()
+                        Button("Submit") {
+                            DataController().addFood(
+                                name: name,
+                                calories: calories,
+                                context: managedObjContext)
+                            dismiss()
+                        }
+                        Spacer()
+                    }
+                }
+        }
+    }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView()
+    }
+}
