373e257fb9568a34200a8043e450518a6c017374
Jonathan Tong <jontong321@gmail.com>
2023-12-28 10:21:16 -0500

    Initial Commit

---
+++ iMacrosTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iMacrosTracker
+//
+//  Created by Jonathan on 2023-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iMacrosTracker/iMacrosTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMacrosTrackerApp.swift
+//  iMacrosTracker
+//
+//  Created by Jonathan on 2023-12-28.
+//
+
+import SwiftUI
+
+@main
+struct iMacrosTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
